6f46dcb846d7d2318de6a2a7c03420f103f7df67
Priya8340 <priyanka90887@gmail.com>
2022-04-01 21:06:58 +0530

    m initial commit

---
+++ src/com/simplilearn/LIS/LIS_1.java
@@ -0,0 +1,37 @@
+package com.simplilearn.LIS;
+
+import java.util.Arrays;
+
+class LIS_1 {
+		static int lis(int arr[], int n)
+	    {
+	        int lis[] = new int[n];
+	        int i, j, max = 0;
+	 
+	        /* Initialize LIS values for all indexes */
+	        for (i = 0; i < n; i++)
+	            lis[i] = 1;
+	 
+	        /* Compute optimized LIS values in
+	           bottom up manner */
+	        for (i = 1; i < n; i++)
+	            for (j = 0; j < i; j++)
+	                if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+	                    lis[i] = lis[j] + 1;
+	 
+	        /* Pick maximum of all LIS values */
+	        for (i = 0; i < n; i++)
+	            if (max < lis[i])
+	                max = lis[i];
+	 
+	        return max;
+	    }
+		public static void main(String args[]) {
+			// Your code goes here
+			int arr[] = {10, 22, 9, 33, 21, 50, 41, 60, 80};
+			int n = 9;
+			System.out.println("From the array: " + Arrays.toString(arr));
+	        System.out.println("\nLength of the longest increasing subsequence: " + (lis(arr, n)));
+		}
+	}
+
